dc40a18018a9f5501241436155f6669fac1b3f6e
Achyuth Varma <achyuth.varma1@gmail.com>
2021-04-03 16:58:50 +0530

    Created Abstract class

--- src/listener.ts
+++ src/listener.ts
@@ -10,6 +10,11 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener Connected to NATS");
 
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
   const options = stan.subscriptionOptions().setManualAckMode(true);
 
   const subscription = stan.subscribe(
@@ -31,3 +36,7 @@ stan.on("connect", () => {
     msg.ack();
   });
 });
+
+// Graceful Shutdown
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
